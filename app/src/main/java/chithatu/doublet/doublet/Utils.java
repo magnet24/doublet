e7f0db1ee62003a9849d9b6d1f7eba3ab4857b89
doubleT <trinhthanhtung.it@gmail.com>
2018-07-10 10:13:53 +0700

    done login

---
+++ app/src/main/java/chithatu/doublet/doublet/Utils.java
@@ -0,0 +1,13 @@
+package chithatu.doublet.doublet;
+
+public class Utils {
+
+    //Email Validation pattern
+    public static final String regEx = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    //Fragments Tags
+    public static final String Login_Fragment = "Login_Fragment";
+    public static final String SignUp_Fragment = "SignUp_Fragment";
+    public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+
+}
